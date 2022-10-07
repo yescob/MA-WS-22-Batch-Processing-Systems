263f4950151e45238c9d746ac9ea35f281bd6151
Jakob <93121637+yescob@users.noreply.github.com>
2022-10-07 10:54:50 +0200

    added NaiveMeanTemperatureReducer

---
+++ HadoopMaxTemperature/src/main/java/at/fhv/fn/naivemeantemperature/NaiveMeanTemperatureReducer.java
@@ -0,0 +1,23 @@
+package at.fhv.fn.naivemeantemperature;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class NaiveMeanTemperatureReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException{
+
+        Integer sumOfAllRecords = 0;      
+        Integer numberOfRecords = 0;
+        for(IntWritable v : values){
+          sumOfAllRecords = sumOfAllRecords + v.get();
+          numberOfRecords++;
+        }
+        context.write(key, new IntWritable(sumOfAllRecords/numberOfRecords));
+        
+    }
+    
+}
